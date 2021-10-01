b1a9c70ad61a4930b919bf5d43fa2e0d1478e8df
RomanPalna <roma.palna@gmail.com>
2021-10-01 16:06:24 +0300

    fix clock

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Clock.css';
 
 const Clock = () => {
   const [time, setTime] = useState(new Date().toLocaleString());
 
   setInterval(() => setTime(new Date().toLocaleString()), 1000);
+
+  useEffect(() => {
+    clearInterval();
+  }, []);
 
   return <div className="Clock__face">{time}</div>;
 };

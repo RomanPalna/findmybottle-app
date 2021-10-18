4d4ad291daf6d365f450975f16ef5d879d00566b
RomanPalna <roma.palna@gmail.com>
2021-10-18 10:17:37 +0300

    Update api-service.js

--- src/api-service/api-service.js
+++ src/api-service/api-service.js
@@ -1,9 +1,11 @@
+import axios from 'axios';
+
 const BASE_URL = 'http://localhost:4040/alcohol';
 
 async function apiService() {
-  const response = await fetch(BASE_URL);
-  return response.ok
-    ? await response.json()
+  const response = await axios.get(BASE_URL);
+  return response
+    ? await response.data
     : Promise.reject(new Error('Not Found'));
 }
 
